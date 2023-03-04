533dc4a5449bd6b9abf57b4743a2faa32910b7b2
Cem Beyazit Karaca Oztekin <cembkoztekin@gmail.com>
2023-03-05 00:44:46 +0300

    feat: prod check for resp

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/discentral/backend/internal/query"
+	"github.com/discentral/backend/pkg/config"
 	"github.com/discentral/backend/pkg/graphiql"
 	"github.com/discentral/backend/pkg/sdl"
 	"github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
+	_ "github.com/joho/godotenv/autoload"
 )
 
 var schema *graphql.Schema
@@ -19,7 +22,11 @@ func init() {
 
 func main() {
 	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write(graphiql.Page)
+		if config.GetWithFallback("PRODUCTION", "false") == "true" {
+			fmt.Fprint(w, "Hello World!")
+		} else {
+			w.Write(graphiql.Page)
+		}
 	}))
 
 	http.Handle("/query", &relay.Handler{Schema: schema})
